0e137e145d54f8d7a70a9c233aa803ab7e8ec78d
Link <linkdoyle@163.com>
2021-07-24 00:09:48 +0800

    [func] Add test `TestMysqlCreate`.

--- db/mysql_test.go
+++ db/mysql_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"testing"
 )
 
@@ -9,4 +10,25 @@ func TestMysqlConnect(t *testing.T) {
 	ConnectMySql()
 	db := GetMySqlDB()
 	fmt.Printf("db: %+v\n", db)
+}
+
+func TestMysqlCreate(t *testing.T) {
+	ConnectMySql()
+	db := GetMySqlDB()
+	user := &User{
+		UserName: "__test",
+		Email:    "__test",
+		NickName: "__test",
+		Password: "__test",
+	}
+	{
+		result := db.Create(user)
+		t.Logf("%+v\n", result.Error)
+	}
+	{
+		var users []User
+		result := db.Find(&users)
+		t.Logf("%+v\n", result.Error)
+		t.Log(spew.Sdump(users))
+	}
 }
