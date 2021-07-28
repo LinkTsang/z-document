3843e7b7e71b52aa9ebf5e93c01cfd9e3a844b62
Link <linkdoyle@163.com>
2021-07-28 17:33:10 +0800

    [func] Update AccountService interface.

--- services/AccountService.go
+++ services/AccountService.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"errors"
+	"gorm.io/gorm"
 	"z-document/models"
 	"z-document/repositories"
 )
@@ -8,22 +10,39 @@ import (
 type AccountService interface {
 	Login(user models.User) error
 	Register(user models.User) error
+	Logout(user models.User) error
 }
 
 type accountService struct {
 	repository repositories.AccountRepository
 }
 
-func (s accountService) Login(user models.User) error {
-	panic("implement me")
-}
-
-func (s accountService) Register(user models.User) error {
-	panic("implement me")
-}
-
 func NewAccountService(accountRepository repositories.AccountRepository) AccountService {
 	return &accountService{
 		repository: accountRepository,
 	}
+}
+
+func (service accountService) Login(user models.User) error {
+	_, err := service.repository.Login(user)
+	return err
+}
+
+func (service accountService) Register(user models.User) error {
+	u, err := service.repository.FindUserByUsername(user.UserName)
+
+	if err == gorm.ErrRecordNotFound {
+		_, err := service.repository.CreateUser(user)
+		return err
+	}
+
+	if err == nil && u.UserName != "" {
+		return errors.New("username already exists")
+	}
+
+	return err
+}
+
+func (service accountService) Logout(user models.User) error {
+	panic("implement me")
 }
